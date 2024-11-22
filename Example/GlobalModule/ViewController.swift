070bcfbf3a8d6595f3d9cc60f01a95c555413d84
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-11-22 17:35:40 +0200

    add logger module file

--- Example/GlobalModule/ViewController.swift
+++ Example/GlobalModule/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let log = LoggerModule()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ LoggerModule.swift
@@ -0,0 +1,18 @@
+//
+//  LoggerModule.swift
+//  GlobalModule
+//
+//  Created by Eslam on 22/11/2024.
+//
+
+import Foundation
+
+public class LoggerModule {
+    public init(){}
+    public func printLog() {
+        print("Hello world")
+    }
+    public func printName() {
+        print("Hi")
+    }
+}
