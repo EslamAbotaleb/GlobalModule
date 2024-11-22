2b89a21506c88e9beb747bf1f6dc3a886a84e923
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-11-22 16:50:13 +0200

    remove extenstion UIIImageView

--- GlobalModule/Classes/ReplaceMe.swift
+++ GlobalModule/Classes/ReplaceMe.swift
@@ -10,11 +10,3 @@ public class LoggerModule {
     }
     
 }
-extension UIImageView {
-   public func circleImageView(borderColor: UIColor, borderWidth: CGFloat){
-       self.layer.borderColor = borderColor.cgColor
-       self.layer.borderWidth = borderWidth
-       self.layer.cornerRadius = self.layer.frame.size.width / 2
-       self.clipsToBounds = true
-   }
-}
