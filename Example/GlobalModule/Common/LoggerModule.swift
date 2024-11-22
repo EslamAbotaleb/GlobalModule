cb75f05c77883bcb27ebe15bbf2acb9bc0eaa018
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-11-22 16:52:25 +0200

    prepare common folder and add logger swift file

---
+++ Example/GlobalModule/Common/LoggerModule.swift
@@ -0,0 +1,16 @@
+//
+//  LoggerModule.swift
+//  GlobalModule_Example
+//
+//  Created by Eslam on 22/11/2024.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import Foundation
+public class LoggerModule {
+    public init(){}
+    public func printLog() {
+        print("Hello world")
+    }
+
+}
--- GlobalModule/Classes/ReplaceMe.swift
+++ GlobalModule/Classes/ReplaceMe.swift
@@ -3,10 +3,3 @@ import Foundation
 import UIKit
 import ObjectiveC
 
-public class LoggerModule {
-    public init(){}
-    public func printLog() {
-        print("Hello world")
-    }
-    
-}
