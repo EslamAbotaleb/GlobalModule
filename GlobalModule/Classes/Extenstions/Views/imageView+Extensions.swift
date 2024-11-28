60c83aab8d5c4329249a11c70b8f8694af3d742c
EslamAbotaleb <eslamabotaleb07@gmail.com>
2024-11-28 18:52:22 +0200

    add extenstion UIImageView

---
+++ GlobalModule/Classes/Extenstions/Views/imageView+Extensions.swift
@@ -0,0 +1,57 @@
+//
+//  imageView+Extensions.swift
+//  CERQEL
+//
+//  Created by Mohamed Karmout on 04/08/2022.
+//  Copyright © 2022 Youxel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var imageCache = [String: UIImage]() // Cache to store images
+
+public extension UIImageView {
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage? {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(origin: .zero, size: newSize)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    func resizedImage(at url: URL, for size: CGSize) -> UIImage?
+    {
+        guard let image = UIImage(contentsOfFile: url.path) else {
+            return nil
+        }
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { (context) in
+            image.draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
+public extension UIImageView {
+    func setImageColor(color: UIColor) {
+        let templateImage = self.image?.withRenderingMode(.alwaysTemplate)
+        self.image = templateImage
+        self.tintColor = color
+    }
+}
