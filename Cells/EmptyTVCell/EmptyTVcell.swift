6bac4d986537096aca09c3c87e8fbc0b6175ee48
EslamAbotaleb <eslamabotaleb07@gmail.com>
2024-12-01 09:43:08 +0200

    Prepare Cells & Utilities folder

---
+++ Cells/EmptyTVCell/EmptyTVcell.swift
@@ -0,0 +1,24 @@
+//
+//  EmptyTVcell.swift
+//  iJusoor
+//
+//  Created by iSlam on 8/7/19.
+//  Copyright © 2019 Youxel. All rights reserved.
+//
+
+import UIKit
+
+class EmptyTVcell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GlobalModule/Classes/Cells/EmptyCVCell/EmptyCVCell.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyCVCell.swift
+//  CERQEL
+//
+//  Created by Eslam on 18/04/2023.
+//  Copyright © 2023 Youxel. All rights reserved.
+//
+
+import UIKit
+
+class EmptyCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
